936516bad453f9da5b14e9054e90d1d2826e666d
Jose Navarrete <janavarretecristino@gmail.com>
2018-01-02 21:12:11 +0100

    Update test

--- zipfsong.py
+++ zipfsong.py
@@ -1,47 +1,91 @@
-#!/usr/bin/python2
+#!/bin/python
 
-""""
-Zipf's song problem
-v1.0 without using any class structure like creating a Song class
-Jose Antonio Navarrete
-@joseanavarrete
+"""
+Zifpsong
+
+https://labs.spotify.com/puzzles/
 """
 
-import sys
+import unittest
+
+class Song:
+    """Song which contains zipf, name and position"""
+    def __init__(self, n_plays, name, position):
+        self.zipf_quality = position*n_plays
+        self.name = name
+        self.position = position
+
+class Album(object):
+    def __init__(self, songs, max_zipfy_songs):
+        self.songs = songs
+        self.max_zipfy_songs = max_zipfy_songs
+    
+    @classmethod
+    def from_multiline_str(cls, options):
+        """
+        Options is a multiline string like:
+        total_songs zipfy_songs
+        n_plays song_name
+        n_plays song_name
+        n_plays song_name
+        """
+        options_list = options.split('\n')
+        total_songs, max_zipfy_songs = map(int, options_list[0].split())
+        album_songs = []
+
+        for song_index in range(1, total_songs+1):
+            n_plays, song_name = options_list[song_index].split()
+            album_songs.append(Song(int(n_plays), song_name, song_index))
+
+        return cls(album_songs, max_zipfy_songs)
+
+    def zipfy(self):
+        zipfied_songs = sorted(
+            self.songs,
+            key=lambda song: song.zipf_quality,
+            reverse=True)[:self.max_zipfy_songs]
+
+        return "\n".join([song.name for song in zipfied_songs])
 
 
-def process_info(n_played, song_name, song_number, songs_array):
-    """
-    Inserts into songs_array song_name processed with its zipf coeficient
-    """
-    zipf = n_played*song_number
-    song = {'zipf': zipf, 'name': song_name, 'song_number': song_number}
-    songs_array.append(song)
+class AlbumZipfyTest(unittest.TestCase):
+    def test_zipfy(self):
+        test_album_1 = """4 2
+30 one
+30 two
+15 three
+25 four"""
+        test_album_2 = """15 3
+197812 re_hash
+78906 5_4
+189518 tomorrow_comes_today
+39453 new_genious
+210492 clint_eastwood
+26302 man_research
+22544 punk
+19727 sound_check
+17535 double_bass
+18782 rock_the_house
+198189 19_2000
+13151 latin_simone
+12139 starshine
+11272 slow_country
+10521 m1_a1"""
 
+        expected_output_1 = """four
+two"""
+        expected_output_2 = """19_2000
+clint_eastwood
+tomorrow_comes_today"""
 
-def my_reverse_multi_sort(x, y):
-    """"
-    Reverse order for zipf and song_number sorting
-    """
-    n_cmp = cmp(y['zipf'], x['zipf'])
-    if n_cmp == 0:
-        return cmp(x['song_number'], y['song_number'])
-    else:
-        return n_cmp
+        album_1 = Album.from_multiline_str(test_album_1)
+        album_2 = Album.from_multiline_str(test_album_2)
 
-
-def sort_and_print_list(songs_array, max_length):
-    returning_list = sorted(songs_array, cmp=my_reverse_multi_sort)[:max_length]
-    print "\n".join([s['name'] for s in returning_list])
-
+        self.assertEqual(album_1.zipfy(), expected_output_1)
+        self.assertEqual(album_2.zipfy(), expected_output_2)
 
 def main():
-    songs_array = []
-    options = map(int, sys.stdin.readline().split())
-    for x in xrange(options[0]):
-        spl_line = map(lambda s: s.strip(), sys.stdin.readline().split())
-        process_info(long(spl_line[0]), spl_line[1], x+1.0, songs_array)
-    sort_and_print_list(songs_array, options[1])
+    unittest.main()
 
 
 if __name__ == '__main__':
